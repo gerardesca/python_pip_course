cdecbc224595fb6abb3d6d4584bc429340722721
Gerardo Jaime <gjaimeescareno@gmail.com>
2023-03-10 18:55:20 -0800

    Add FastAPI and uvicornmodules to web_server project

--- web_server/main.py
+++ web_server/main.py
@@ -1,4 +1,20 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_numbers():
+    return [1,2,3]
+
+@app.get('/pagina', response_class = HTMLResponse)
+def get_contact():
+    return """
+        <h1>Mi primer pagina con FastAPI</h1>
+        <p>Mi primer API</p>
+    """
+
 
 def run():
     store.get_categories()
